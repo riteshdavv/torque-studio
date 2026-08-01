7c5a84a550fa52c06850a1b33ffb6fa0be39c76f
Ritesh Singh <ritesh.davv@gmail.com>
2026-08-02 02:52:41 +0530

    Added Google Analytics tag

--- app/layout.tsx
+++ app/layout.tsx
@@ -38,6 +38,21 @@ export default function RootLayout({
           </SmoothScrollProvider>
         </ThemeProvider>
         <Script src="https://assets.calendly.com/assets/external/widget.js" strategy="lazyOnload" />
+        
+        {/* Google Analytics */}
+        <Script
+          src="https://www.googletagmanager.com/gtag/js?id=G-YSP1J63312"
+          strategy="afterInteractive"
+        />
+        <Script id="google-analytics" strategy="afterInteractive">
+          {`
+            window.dataLayer = window.dataLayer || [];
+            function gtag(){dataLayer.push(arguments);}
+            gtag('js', new Date());
+
+            gtag('config', 'G-YSP1J63312');
+          `}
+        </Script>
       </body>
     </html>
   )
